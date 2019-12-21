ad98960e6bdc470337b9bfdd6d44f97dd3d96068
Orion55 <grol55@mail.ru>
2019-12-21 21:38:27 +0500

    first commit

---
+++ 311kvit.ps1
@@ -0,0 +1,31 @@
+﻿#Программа проверки квитанций по форме 311 для юр. лиц
+#от 21.12.2019
+
+#текущий путь
+$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+[string]$lib = "$curDir\lib"
+. $curDir/variables.ps1
+. $lib/PSMultiLog.ps1
+. $lib/libs.ps1
+
+Set-Location $curDir
+
+Clear-Host
+
+Start-HostLog -LogLevel Information
+Start-FileLog -LogLevel Information -FilePath $logName -Append
+
+testDir(@($noticePath))
+createDir(@($logPath))
+
+if ($debug) {
+	Remove-Item -Path "$noticePath\*.*"
+	Copy-Item -Path "$curDir\OUT1\*.*" -Destination $noticePath
+}
+
+
+Write-Log -EntryType Information -Message "Завершение обработки..."
+
+Stop-FileLog
+Stop-HostLog
---
+++ variables.ps1
@@ -0,0 +1,36 @@
+﻿[string]$curDir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+[boolean]$debug = $true
+
+#каталог с извещениями
+[string]$noticePath = "$curDir\OUT"
+
+#рабочий временный каталог
+[string]$tmp = "$curDir\temp"
+
+#настройка почты
+[string]$311mailAddrFiz = "tmn-goe@tmn.apkbank.ru"
+#[string]$311mailAddrFiz = "tmn-f311@tmn.apkbank.apk"
+$311mailAddrJur = "tmn-goe <tmn-goe@tmn.apkbank.ru>", "lma <lma@tmn.apkbank.ru>"
+#[string[]]$311mailAddrJur = "<tmn-lov@tmn.apkbank.ru>", "<tmn_oit@tmn.apkbank.apk>"
+
+[string]$mailServer = "191.168.6.50"
+[string]$mailFrom = "atm_support@tmn.apkbank.apk"
+
+#входящие - настройки
+[string]$311MaskFiz = "^NN.+\.arj$"
+[string]$311MaskJur = "^ON.+\.arj$"
+[string]$311MaskJur2 = "^S.+\.arj$"
+
+[string]$311Archive = "$tmp\311p\Arhive"
+[string]$311DirKvit = "$tmp\311pMsk"
+[string]$311JurArchive = "$tmp\311jur\Arhive"
+
+$curDate = Get-Date -Format "ddMMyyyy"
+[string]$logPath = "$curDir\log"
+#имя лог-файла
+[string]$logName = $logPath + '\' + $curDate + "_kvit.log"
+
+[string]$spki = "C:\Program Files\MDPREI\spki\spki1utl.exe"
+[string]$vdkeys = "d:\SKAD\Floppy\foiv"
+[string]$profile = "r2880_2"
+[string]$logSpki = $curDir + "\log\" + $curDate + "_spki_tr.log"
